6b2dc72f7dc3fd9855b66591b11c3d700b08036a
Maringngerrang <maringngerrang@gmail.com>
2021-05-31 02:02:11 +0800

    Update start_server.bat
    Memperbaiki mekanisme start server melalui batch file

--- start_server.bat
+++ start_server.bat
@@ -1,1 +1,3 @@
-php artisan serve 
+START "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\XAMPP\XAMPP Control Panel.lnk"
+START "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio Code\Visual Studio Code.lnk"
+php artisan serve
